ade0542228b1ac8ea7f5ca91a57ef9065c497e0d
kruskallin <kruskallin@tamu.edu>
2022-03-19 22:37:48 -0500

    info

--- pr04/script.js
+++ pr04/script.js
@@ -1,6 +1,3 @@
-// WebGL - 3D Step 2 - Use 3D Geometry
-// from https://webglfundamentals.org/webgl/webgl-3d-step2.html
-
 "use strict";
 
 function main() {
@@ -22,6 +19,10 @@ function main() {
     var colorLocation = gl.getUniformLocation(program, "u_color");
     var pe = gl.getUniformLocation(program, "pe");
     var r = gl.getUniformLocation(program, "r");
+    var bright = gl.getUniformLocation(program, "bright");
+    var dark = gl.getUniformLocation(program, "dark");
+    var sharp = gl.getUniformLocation(program, "sharp");
+    var lightColor = gl.getUniformLocation(program, "color");
 
 
     // Create a buffer to put positions in
@@ -60,8 +61,12 @@ function main() {
     // set the uniforms
     var color = [0, 0, 0, 1];
     gl.uniform4fv(colorLocation, color);
-    gl.uniform3fv(pe, [0.0, 0.0, 1]);   //default camera position
+    gl.uniform3fv(pe, [0.0, 0.0, 1.0]);   //default camera position
     gl.uniform1f(r, 0.2);             //default radius of sphere
+    gl.uniform1f(bright, 0.8);             //default radius of sphere
+    gl.uniform1f(dark, 0.2);             //default radius of sphere
+    gl.uniform1f(sharp, 0.2);             //default radius of sphere
+    gl.uniform3fv(lightColor, [1.0, 1.0, 1.0]);   //default camera position
 
 
     // Draw the geometry.
@@ -74,19 +79,44 @@ function main() {
     var y_slider = document.querySelector('#y');
     var z_slider = document.querySelector('#z');
     var r_slider = document.querySelector('#r');
+    var bright_slider = document.querySelector('#bright');
+    var dark_slider = document.querySelector('#dark');
+    var sharp_slider = document.querySelector('#sharp');
+    var color_picker = document.querySelector('#color');
 
     x_slider.addEventListener("input", update);
     y_slider.addEventListener("input", update);
     z_slider.addEventListener("input", update);
     r_slider.addEventListener("input", update);
+    bright_slider.addEventListener("input", update);
+    dark_slider.addEventListener("input", update);
+    sharp_slider.addEventListener("input", update);
+    color_picker.addEventListener("input", update);
 
     function update() {
         gl.uniform3fv(pe, [x_slider.value, y_slider.value, z_slider.value]);
         gl.uniform1f(r, r_slider.value);
+        gl.uniform1f(bright, bright_slider.value);
+        gl.uniform1f(dark, dark_slider.value);
+        gl.uniform1f(sharp, sharp_slider.value);
+
+        var rgb = hexToRgb(color_picker.value)
+        gl.uniform3fv(lightColor, [rgb.r / 255.0, rgb.g / 255.0, rgb.b / 255.0]);
+
         gl.drawArrays(primitiveType, offset, count);
     }
 
 }
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
 
 // Fill the canvas with black.
 function setGeometry(gl) {
